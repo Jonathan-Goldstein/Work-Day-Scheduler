e2b51e57a50d239eff87a2d675f3f2963eaa0481
Jonathan Goldstein <jonathangoldsteine@gmail.com>
2021-01-27 20:15:17 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,6 @@ let currentDay = document.getElementById('currentDay');
 let DateTime = luxon.DateTime;
 //gives me a date time object that gives me the current date and time on the user's computer
 let today = DateTime.local();
+
+//setting the text content for current day
+currentDay.textContent = today.toLocaleString(DateTime.DATE_HUGE);
