8e034808aab6eea387b35a7a7424d5bdaf425769
Jonathan Goldstein <jonathangoldsteine@gmail.com>
2021-03-29 14:10:29 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,86 @@
-//setting the value for current day
-let currentDay = document.getElementById('currentDay');
-//luxon variable shortcut
+// Setting the variables
 let DateTime = luxon.DateTime;
-//gives me a date time object that gives me the current date and time on the user's computer
 let today = DateTime.local();
+let hourNow = today.hour;
+console.log(hourNow);
+console.log(today);
+// Variable array containing the hours
+var hours = [
+    {hour: "9am",
+     mhour: "9"},
+    {hour: "10am",
+     mhour: "10"},
+    {hour: "11am",
+     mhour: "11"},
+    {hour: "12pm",
+     mhour: "12"},
+    {hour: "1pm",
+     mhour: "13"},
+    {hour: "2pm",
+     mhour: "14"},
+    {hour: "3pm",
+     mhour: "15"},
+    {hour: "4pm",
+     mhour: "16"},
+    {hour: "5pm",
+     mhour: "17"}, 
+];
+$(document).ready(function() {
+    $("#currentDay").text(today.toLocaleString(DateTime.DATE_FULL));
 
-//setting the text content for current day
-currentDay.textContent = today.toLocaleString(DateTime.DATE_HUGE);
+    //Container class
+    $(".container").addClass("time-block");
+
+    hours.forEach(function(hours) {
+      //appends rows to container - everything below additionally appends
+        var row = $("<article>").addClass("row");
+        $(".container").append(row);
+      
+        var timeSec = $("<section>").addClass("hour col-2 pt-4")
+        timeSec.text(hours.hour);
+        row.append(timeSec);
+        var textSec = $("<textarea>").addClass("textarea col-8 description")
+        row.append(textSec);
+         var btnSec = $("<button>").addClass("saveBtn col-2")
+        row.append(btnSec);
+        var img = $("<i>").addClass("far fa-save fa-lg")
+        btnSec.append(img);
+
+        // If else statements
+        if (hourNow < hours.mhour) {
+            $(textSec).addClass("future");
+        }
+
+        else if (hourNow > hours.mhour) {
+            $(textSec).addClass("past");
+        }
+
+        else {
+            $(textSec).addClass("present");
+        }
+
+        // Storage variable
+        var key = hours.hour;
+        console.log(key);
+
+        // Sets the box text
+        var toDo = localStorage.getItem(key);
+        console.log(toDo);
+        $(textSec).val(toDo);
+
+    });
+
+    $(document).on('click', '.row', function() {
+        
+        console.log($(this));
+    
+        // Storage use variables
+        var key = $(this)[0].firstChild.firstChild.data;
+        console.log(key);
+        var value = $(this)[0].firstElementChild.nextSibling.value;
+    
+        localStorage.setItem(key, value);
+    
+    })
+
+});
